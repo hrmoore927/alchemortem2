9a0c94d0ed66be7f5057377240491c7b3738d67d
hrmoore927 <hrmoore927@gmail.com>
2018-03-01 11:05:48 -0500

    Successful shopping cart view

--- resources/views/shop/cart.blade.php
+++ resources/views/shop/cart.blade.php
@@ -5,35 +5,48 @@ Cart - Alchemortem
 @endsection
 
 @section('content')
-    @if(Session::has('cart))
+    @if(Session::has('cart'))
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-sm-6 col-md-12">
                 <table class="table">
                     <thead>
                         <tr>
-                          <th scope="col">Product</th>
+                          <th scope="col">Product Name</th>
                           <th scope="col">Quantity</th>
                           <th scope="col">Price</th>
+                          <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach($products as $product)
                         <tr>
-                          <td>{{ $product['item']['title'] }}</td>
-                          <td>{{ $product['qty'] }}</td>
-                          <td>{{ $product['price'] }}</td>
+                            <td>{{ $product['item']['productName'] }}</td>
+                            <td>{{ $product['qty'] }}</td>
+                            <td>{{ $product['price'] }}</td>
+                            <td>Action</td>
                         </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
-    @else
-    <div class="row">
-        <div class="col-md-12">
-            <h2>No items in cart.</h2>
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <strong>Total: {{ $totalPrice }}</strong>
+            </div>
         </div>
-    </div>
+        <hr>
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <a href="" class="btn btn-success" type="button">Checkout</a>
+            </div>
+        </div>
+    @else
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <h2>No items in cart</h2>
+            </div>
+        </div>
     @endif
 
 @endsection
