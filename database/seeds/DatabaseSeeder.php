309d9490bc198f6711ee961019a1e560803a7c0d
hrmoore927 <hrmoore927@gmail.com>
2018-04-26 10:38:29 -0400

    Removed shipping and payment tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            PaymentTableSeeder::class,
             ProductTableSeeder::class
         ]);
     }
